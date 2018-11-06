1b19ff84db20fe934b64449eca705ba432727893
Patricia Martinez <patricia.martinez@imatia.com>
2018-11-06 16:21:40 +0100

    Fixed comands

--- bin/ontimize-web-ngx-library.js
+++ bin/ontimize-web-ngx-library.js
@@ -9,28 +9,26 @@ if (!shell.which('git')) {
   shell.exit(1);
 }
 
-yargs.command("build", "building dist folder", function (yargs) {
-  // var args = yargs.argv;
-  runBuild();
-});
-
-yargs.command("clean", "clean library assets", function (yargs) {
-  cleanLibraryAssets();
-});
-
 var argv = yargs.usage("$0 command")
-  .demand(1, "must provide a valid command")
-  .option("package", {
-    alias: "package",
-    demand: true,
-    describe: "package name",
-    type: "string"
+  .command("build", "building dist folder", function (yargsBuild) {
+    yargsBuild.option("package", {
+      alias: "package",
+      demand: true,
+      describe: "package name",
+      type: "string"
+    })
+      .option("ng-library", {
+        alias: "ng-library",
+        demand: true,
+        describe: "package library name (ng.ontimizeWeb)",
+        type: "string"
+      });
+
+    runBuild();
   })
-  .option("ng-library", {
-    alias: "ng-library",
-    demand: true,
-    describe: "package library name (ng.ontimizeWeb)",
-    type: "string"
+  .demand(1, "must provide a valid command")
+  .command("clean", "clean library assets", function (yargs) {
+    cleanLibraryAssets();
   })
   .help("h")
   .alias("h", "help")
